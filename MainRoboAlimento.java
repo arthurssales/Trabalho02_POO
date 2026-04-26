6b62d53243cef29663f7cfff50c43533f672f6ea
arthurssales <arthursales2805@gmail.com>
2026-04-26 02:54:36 -0300

    Atualizar o MainRoboAlimento.java

--- MainRoboAlimento.java
+++ MainRoboAlimento.java
@@ -17,13 +17,15 @@ opcao = teclado.nextInt();
 teclado.nextLine();
 
        while(true){
-        System.out.println("1 - Azul\n2 - Vermelho\n3 - Preto\n4 - Branco: ");
-        corRobo = teclado.nextInt();
-        
-
-      Robo robo = new Robo(corRobo);
-    
-      }
+        System.out.println("Azul\nVermelho\nPreto\nBranco: ");
+       try{
+           corRobo = teclado.nextLine();       
+           Robo robo = new Robo(corRobo);   
+           break;
+       }catch(CoordenadaInvalidaException  e){ 
+   System.out.println(e.getMessage()
+}
+   }   
 
         while(!posicaoValida){
            
